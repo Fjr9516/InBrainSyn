6acd7bbc865c7aa5dd147bfd05d8798d0f7df602
jingruf <jingruf@kth.se>
2024-12-10 21:14:41 +0100

    update paths

--- step2_parallel_transport_using_SchildsLadder_example.sh
+++ step2_parallel_transport_using_SchildsLadder_example.sh
@@ -3,10 +3,10 @@
 subj="OAS30331"
 for i in {1..3}; do
 # Define the input and output file paths with {x} replaced by the current value of i
-input_mask="/usr/src/myapp/volume/data/${subj}/I0_MASK.nii.gz"
-inter_SVF="/usr/src/myapp/volume/data/${subj}/inter_SVF_half.nii.gz"
-intra_SVF="/usr/src/myapp/volume/data/${subj}/intra_SVF_half_${i}.nii.gz"
-output_transported_SVF="/usr/src/myapp/volume/data/${subj}/transported_SVF_half_${i}.nii.gz"
+input_mask="/usr/src/myapp/volume/${subj}/I0_MASK.nii.gz"
+inter_SVF="/usr/src/myapp/volume/${subj}/inter_SVF_half.nii.gz"
+intra_SVF="/usr/src/myapp/volume/${subj}/intra_SVF_half_${i}.nii.gz"
+output_transported_SVF="/usr/src/myapp/volume/${subj}/transported_SVF_half_${i}.nii.gz"
 
 # Run the command with the current values of input and output file paths
 ./SchildsLadder -m "$input_mask" -v "$intra_SVF" -d "$inter_SVF" -t "$output_transported_SVF"
